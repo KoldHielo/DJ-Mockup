49bd8ebacf22e40e368c7e5a9a4716be94e4384e
Kieran Oldfield <info@kieranoldfield.co.uk>
2024-01-30 18:04:37 +0000

    menu complete - project can be called finished

--- static/script.js
+++ static/script.js
@@ -1,4 +1,8 @@
 const decksImgTwo = document.querySelector('#decks-img-two');
+const menuButton = document.querySelector('#menu-button');
+const menuButtonBars = menuButton.querySelectorAll('.bar');
+const menuAnchors = document.querySelectorAll('#menu a');
+const menu = document.querySelector('#menu');
 
 const scrollFunc = () => {
     const viewportBottom = window.scrollY + window.innerHeight;
@@ -9,4 +13,32 @@ const scrollFunc = () => {
     }
 }
 
+const menuClickFunc = () => {
+    if(menu.hasAttribute('style')) {
+        menu.removeAttribute('style');
+    }
+
+    else {
+        menu.style.opacity = 1;
+        menu.style.top = 0
+    }
+}
+
+
+menuAnchors.forEach(anchor => {
+    anchor.addEventListener('click', e => {
+      e.preventDefault();
+      menu.removeAttribute('style');
+      const targetId = anchor.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+      const targetPosition = targetElement.getBoundingClientRect().top + window.scrollY - 40;
+      window.scrollTo({
+        top: targetPosition,
+        left: 0,
+        behavior: 'smooth'
+      });
+    })
+  })
+
 window.addEventListener('scroll', scrollFunc);
+menuButton.addEventListener('click', menuClickFunc);
